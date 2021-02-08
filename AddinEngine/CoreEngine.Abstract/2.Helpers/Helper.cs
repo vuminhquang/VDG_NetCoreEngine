6842612ad78596e99a52104d0c1d3e731a01ffd0
quangv <vu.minh.quang@outlook.com>
2021-02-08 16:17:48 +0700

    Add helper function on types

---
+++ AddinEngine/CoreEngine.Abstract/2.Helpers/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AddinEngine.Abstract._2.Helpers
+{
+    public static class Helper
+    {
+        public static IEnumerable<Type> GetAllTypesThatImplementInterface<T>(this Assembly assembly)
+        {
+            return assembly
+                .GetTypes()
+                .Where(type => typeof(T).IsAssignableFrom(type) && !type.IsInterface);
+        }
+
+        public static bool IsTypeRegistered<T>(this IDependencyRegister services, Type type)
+        {
+            var tDescriptor = services.FirstOrDefault(s => s.ServiceType == type);
+            return tDescriptor != null;
+        }
+    }
+}
